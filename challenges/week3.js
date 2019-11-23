b06309892f9dad8b4fa0303f3ede99202d0e7e09
Karen <karenbrady92@hotmail.co.uk>
2019-11-23 18:52:24 +0200

    completed Task 1 getSqures challenge

--- challenges/week3.js
+++ challenges/week3.js
@@ -1,6 +1,6 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  return nums.map(num => num * num);
 }
 
 function camelCaseWords(words) {
