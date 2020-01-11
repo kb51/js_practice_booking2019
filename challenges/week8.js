bc7016c16f27546febc753e6e67af8491e6ed356
Karen <karenbrady92@hotmail.co.uk>
2020-01-11 15:50:22 +0000

    completed findNeedle challenge

--- challenges/week8.js
+++ challenges/week8.js
@@ -44,8 +44,7 @@ const sumArrays = arrs => {
 const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
   if (arr.length >= 2) {
-  [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
-  return arr;
+    [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
   }
   return arr;
 };
@@ -53,12 +52,17 @@ const arrShift = arr => {
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  return Object.values(haystack).some(val => {
+    if (typeof val === 'string') {
+      return val.toLowerCase().includes(searchTerm.toLowerCase())
+    }
+  })
 };
 
 const getWordFrequencies = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+
+
 };
 
 module.exports = {
