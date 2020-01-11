62b8f1068e75a2629a16448f390e179bb0f071e7
Karen <karenbrady92@hotmail.co.uk>
2020-01-11 01:11:45 +0000

    completed arrShift challenge

--- challenges/week8.js
+++ challenges/week8.js
@@ -43,7 +43,11 @@ const sumArrays = arrs => {
 
 const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  if (arr.length >= 2) {
+  [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+  return arr;
+  }
+  return arr;
 };
 
 const findNeedle = (haystack, searchTerm) => {
